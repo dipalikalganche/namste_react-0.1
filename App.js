cdb7f838f6b049e37f61155dd4e26650f12f658f
dipali kalganche <dipalikalganche@gmail.com>
2026-07-09 01:11:38 +0530

    episode 3 of namaste react

--- App.js
+++ App.js
@@ -1,32 +1,25 @@
-/**
- * //nested html structure 
- * 
- * <div id="parent">
- *    <div id = "child">
- *       <h1>I am h1 tag </h1>
- *        <h2>I am h2 tag </h2>
- * </div>
- * 
- */ 
+import React from "react";
+import ReactDOM from "react-dom/client"; 
+                                                                 
+//jsx (transpiled before it reaches the js) -parcel-Babel
 
-const parent = React.createElement(
-    "div",
-    {id:"parent"},
-    React.createElement("div", {id:"child"},[
-    React.createElement("h1",{},"I am h1 tag"),
-    React.createElement("h2",{},"I am h2 tag"),
-    ])
+//jsx =>  babel is transpiles it to React.createElement => ReactElement -Js object =>HTMLElement(render)
+
+//React element
+const heading =(
+   <h1 className="head" tabIndex="5">
+        Namaste React using JSX
+   </h1>
 );
- console.log(parent);
- 
-//  const heading = React.createElement(
-//     "h1",
-//     {id:"heading"},
-//     "hello world from react inside it"
-// );
 
-//console.log(heading);// return object
-         
- const root = ReactDOM.createRoot(document.getElementById("root"));
+//React functional Component 
+const HeadingComponent = () => (
+   <div id="container">
+       <h1 className="heading">namaste React Functional Component</h1>
+   </div>  
+);
 
- root.render(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent/>);
---
+++ dist/namaste_react.6bd02f5a.js
@@ -0,0 +1,28 @@
+/**
+ * //nested html structure 
+ * 
+ * <div id="parent">
+ *    <div id = "child">
+ *       <h1>I am h1 tag </h1>
+ *        <h2>I am h2 tag </h2>
+ * </div>
+ * 
+ */ const parent = React.createElement("div", {
+    id: "parent"
+}, React.createElement("div", {
+    id: "child"
+}, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag")
+]));
+console.log(parent);
+//  const heading = React.createElement(
+//     "h1",
+//     {id:"heading"},
+//     "hello world from react inside it"
+// );
+//console.log(heading);// return object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=namaste_react.6bd02f5a.js.map
